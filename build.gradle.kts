8b5a0806ea2f59d8750c508285d9dc76f1f5dac7
juliakononov <julickk04@mail.ru>
2024-08-26 21:54:40 +0300

    MVP

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+val kotlin_version: String by project
+val logback_version: String by project
+val ktor_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.0.0"
+    id("io.ktor.plugin") version "2.3.12"
+    kotlin("plugin.serialization") version "1.8.0"
+}
+
+group = "com.monitoring"
+version = "0.0.1"
+
+application {
+    mainClass.set("com.monitoring.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-server-html-builder:$ktor_version")
+}
